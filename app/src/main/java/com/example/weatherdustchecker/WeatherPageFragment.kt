e7bfc0c4de3395dc12d04f66ecf07aeab4e76204
eunjin2118 <ally6324@naver.com>
2022-07-27 09:24:23 +0900

    arguments 값 참조해서 두 개 값 가져오고, 해당하는 뷰에 출력해주기

--- app/src/main/java/com/example/weatherdustchecker/WeatherPageFragment.kt
+++ app/src/main/java/com/example/weatherdustchecker/WeatherPageFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 
 class WeatherPageFragment : Fragment(){
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,6 +18,14 @@ class WeatherPageFragment : Fragment(){
         val view = inflater
             .inflate(R.layout.weather_page_fragment,
                 container, false)
+
+        // TODO: arguments 값 참조해서 두 개 값 가져오고, 해당하는 뷰에 출력해주기
+        val statusText = view.findViewById<TextView>(R.id.weather_status_text)
+        val temperatureText = view.findViewById<TextView>(R.id.weather_temp_text)
+
+        statusText.text = arguments?.getString("status")
+        temperatureText.text = arguments?.getString("temperature").toString()
+
         return view
     }
 
